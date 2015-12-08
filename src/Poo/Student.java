b7ed0c0d194c8ccf9f122fd829a695b212b0c644
PC ASUS <PC ASUS@PCAZUS>
2015-12-08 22:03:17 +0100

    inheritance student and teacher

--- src/Poo/Student.java
+++ src/Poo/Student.java
@@ -1,9 +1,63 @@
 package Poo;
 
-public class Student extends Teacher{
+import java.util.ArrayList;
 
-	public Student() {
-		// TODO Auto-generated constructor stub
+public class Student extends Person{
+	
+	private String email;
+	private String classStudent;
+	//ArrayList declarations
+	private ArrayList notes;
+	private ArrayList cours;
+	
+	
+	public Student(String name, String firstname, String city, int age,String email,String classStudent){
+		super(name,firstname,city,age);
+		this.email=email;
+		this.classStudent=classStudent;
+		notes=new ArrayList();
+		cours=new ArrayList();	
 	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getClassStudent() {
+		return classStudent;
+	}
+
+
+	public void setClassStudent(String classStudent) {
+		this.classStudent = classStudent;
+	}
+
+
+	public ArrayList getNotes() {
+		return notes;
+	}
+
+
+	public void setNotes(ArrayList notes) {
+		this.notes = notes;
+	}
+
+
+	public ArrayList getCours() {
+		return cours;
+	}
+
+
+	public void setCours(ArrayList cours) {
+		this.cours = cours;
+	}
+
 
 }
