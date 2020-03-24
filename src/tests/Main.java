fcdf01b0a775354577ba65a81d24ed3f69ddd465
YasserTahri <yesser.tahri@gmail.com>
2020-03-24 21:48:31 +0100

    test commit proj

--- src/tests/Main.java
+++ src/tests/Main.java
@@ -7,15 +7,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		 
-		//Person p1 = new Person("tahri", "yasser", "ariana", 26);
+		Person p1 = new Person("tahri", "yasser", "ariana", 26);
 		Teacher teach = new Teacher("Tounis", "MOHAMED", "Tunis", 45, "Docteur", "MATH");
 		teach.display();
 		//p1.display();
-		//Person p1 = new Person();
+		Person p1 = new Person();
 	
 
-		//p1.afficher();
-		//p1.toString();
+		p1.afficher();
+		p1.toString();
 		//p1.setName("TAHRI");
 		//p1.setFirstname("YASSER");
 		
